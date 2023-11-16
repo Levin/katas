1f94d823bf968d636478eb638f3a7e546c95a6f8
levin <mail.an.levin@gmail.com>
2023-11-16 15:15:51 +0100

    binary search with a random twist

--- karachop/lib/tropper.ex
+++ karachop/lib/tropper.ex
@@ -1,5 +1,9 @@
-defmodule Chopper do
+defmodule Tropper do
   
+  @moduledoc """
+    this type of binary search makes the normal trunctation of the left/right part It then makes a short random search by generating a random number and checking that index in the remaining list.
+  """
+
   use GenServer
   require Logger
 
@@ -10,13 +14,12 @@ defmodule Chopper do
   def status() do  
     GenServer.call(__MODULE__, :info)
   end
-  
+
   def start(list, search) do
     params = %{list: list, search: search, found: false}
     GenServer.start_link(__MODULE__, params, name: __MODULE__)
   end
 
-  @impl true
   def init(params) do
     {:ok, params}
   end
@@ -30,23 +33,35 @@ defmodule Chopper do
       mid_num == status.search -> 
         Logger.debug("currently compared #{mid_num} with #{status.search}")
         {:noreply, %{status | list: [mid_num], found: true}}
-      mid_num > status.search -> 
+      mid_num > status.search ->
         Logger.debug("currently compared #{mid_num} with #{status.search}")
         new_list = Enum.slice(status.list, 0, div(length(status.list), 2))
-        {:noreply, %{status | list: new_list}}
+        case random_check(new_list) == status.search do
+          true -> {:noreply, %{status | list: [mid_num], found: true}}
+          _ -> {:noreply, %{status | list: new_list}}
+        end
       mid_num < status.search -> 
         Logger.debug("currently compared #{mid_num} with #{status.search}")
         new_list = Enum.slice(status.list, div(length(status.list), 2), length(status.list))
-        {:noreply, %{status | list: new_list}}
+        case random_check(new_list) == status.search do
+          true -> {:noreply, %{status | list: [mid_num], found: true}}
+          _ -> {:noreply, %{status | list: new_list}}
+        end
     end
 
-      
   end
 
-
-  ef handle_call(:info, _from, state) do
+  def handle_call(:info, _from, state) do
     {:reply,state,  state}
   end
+
+  defp random_check(list) do
+    list_length = length(list)
+    rand_num = :random.uniform(list_length)
+    Logger.debug("[#{__MODULE__}] is testing this number now: #{Enum.at(list, rand_num)}")
+    Enum.at(list, rand_num)
+  end
+
 
 
 
