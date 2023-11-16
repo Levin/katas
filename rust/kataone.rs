761010c70f83fb17207efc8db9080171c338e06b
levin <mail.an.levin@gmail.com>
2023-11-16 08:31:35 +0100

    supermarket pricing

---
+++ rust/kataone.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let data = 
+    "
+    1000
+    2000
+    3000
+
+    4000
+
+    5000
+    6000
+
+    7000
+    8000
+    9000
+
+    10000
+    ";
+
+    let raw = data.split("\n");
+    for calorie in raw 
+    {
+            if calorie == "" 
+            {
+
+            }
+    }
+
+
+    for s in raw {
+    println!("{}", s);
+    }
+}   
